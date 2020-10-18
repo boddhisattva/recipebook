e535bb664aa6261222b05fc91645fad32e66b761
Mohnish G J <mail4mohnish@gmail.com>
2020-10-18 03:54:23 +0200

    Replace record new episodes with allow HTTP conn when no cassette
    - In certain cases, since the list_recipes.yml cassette was not proving sufficient enough to cover the test case scenarios(it was making HTTP calls), it was most likely trying to record new interactions, hence replacing the new_episode option with allow_http_connections_when_no_cassette = true

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,10 +71,10 @@ end
 
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.allow_http_connections_when_no_cassette = true
   config.hook_into :webmock
   config.ignore_localhost = true
   config.configure_rspec_metadata!
-  config.default_cassette_options = { record: :new_episodes }
   config.filter_sensitive_data('<CONTENTFUL_ACCESS_TOKEN>') { ENV.fetch('CONTENTFUL_ACCESS_TOKEN') }
   config.filter_sensitive_data('<CONTENTFUL_SPACE_ID') { ENV.fetch('CONTENTFUL_SPACE_ID') }
   config.filter_sensitive_data('<CONTENTFUL_ENVIRONMENT') { ENV.fetch('CONTENTFUL_ENVIRONMENT') }
