62c7d599000d1d8ea01a5187ca5d913c0a06ff0e
Mohnish G J <mail4mohnish@gmail.com>
2020-10-20 07:47:44 +0200

    Add spec to ensure appropriate text is displayed in case no recipes

---
+++ spec/view/index.html.erb_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'recipes/index.html.erb', type: :view do
+  context 'when no recipes are present' do
+    it 'displays no recipes available related message', vcr: { cassette_name: 'list_recipes' } do
+      @recipes = Recipe.all.load
+      allow(@recipes).to receive(:map) { [] }
+
+      render template: 'recipes/index.html.erb'
+
+      expect(rendered).to have_text('There are no recipes that are currently available')
+    end
+  end
+end
