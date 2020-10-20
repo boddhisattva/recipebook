06a32a313209ba151d18bc247aabfb404a248fe0
Mohnish G J <mail4mohnish@gmail.com>
2020-10-20 15:58:48 +0200

    Fix webpack not found issue with Heroku
    Reference: https://stackoverflow.com/a/39948272/272398
    
    Additionally:
    - Use rubocop to fix multiline block with do end

--- spec/system/recipes_spec.rb
+++ spec/system/recipes_spec.rb
@@ -1,19 +1,23 @@
 require 'rails_helper'
 
 describe 'Recipes', type: :system do
-  let(:recipe_titles) { [
-    'White Cheddar Grilled Cheese with Cherry Preserves & Basil',
-    'Tofu Saag Paneer with Buttery Toasted Pita',
-    'Grilled Steak & Vegetables with Cilantro-Jalapeño Dressing',
-    'Crispy Chicken and Rice with Peas & Arugula Salad'
-  ]}
+  let(:recipe_titles) do
+    [
+      'White Cheddar Grilled Cheese with Cherry Preserves & Basil',
+      'Tofu Saag Paneer with Buttery Toasted Pita',
+      'Grilled Steak & Vegetables with Cilantro-Jalapeño Dressing',
+      'Crispy Chicken and Rice with Peas & Arugula Salad'
+    ]
+  end
 
-  let(:recipe_image_urls) { [
-    'http://images.ctfassets.net/kk2bw5ojx476/61XHcqOBFYAYCGsKugoMYK/0009ec560684b37f7f7abadd66680179/SKU1240_hero-374f8cece3c71f5fcdc939039e00fb96.jpg',
-    'http://images.ctfassets.net/kk2bw5ojx476/48S44TRZN626y4Wy4CuOmA/9c0a510bc3d18dda9318c6bf49fac327/SKU1498_Hero_154__2_-adb6124909b48c69f869afecb78b6808-2.jpg',
-    'http://images.ctfassets.net/kk2bw5ojx476/3TJp6aDAcMw6yMiE82Oy0K/2a4cde3c1c7e374166dcce1e900cb3c1/SKU1503_Hero_102__1_-6add52eb4eec83f785974ddeac3316b7.jpg',
-    'http://images.ctfassets.net/kk2bw5ojx476/5mFyTozvSoyE0Mqseoos86/fb88f4302cfd184492e548cde11a2555/SKU1479_Hero_077-71d8a07ff8e79abcb0e6c0ebf0f3b69c.jpg'
-  ]}
+  let(:recipe_image_urls) do
+    [
+      'http://images.ctfassets.net/kk2bw5ojx476/61XHcqOBFYAYCGsKugoMYK/0009ec560684b37f7f7abadd66680179/SKU1240_hero-374f8cece3c71f5fcdc939039e00fb96.jpg',
+      'http://images.ctfassets.net/kk2bw5ojx476/48S44TRZN626y4Wy4CuOmA/9c0a510bc3d18dda9318c6bf49fac327/SKU1498_Hero_154__2_-adb6124909b48c69f869afecb78b6808-2.jpg',
+      'http://images.ctfassets.net/kk2bw5ojx476/3TJp6aDAcMw6yMiE82Oy0K/2a4cde3c1c7e374166dcce1e900cb3c1/SKU1503_Hero_102__1_-6add52eb4eec83f785974ddeac3316b7.jpg',
+      'http://images.ctfassets.net/kk2bw5ojx476/5mFyTozvSoyE0Mqseoos86/fb88f4302cfd184492e548cde11a2555/SKU1479_Hero_077-71d8a07ff8e79abcb0e6c0ebf0f3b69c.jpg'
+    ]
+  end
 
   describe 'List recipes', vcr: { cassette_name: 'list_recipes' } do
     context 'when a user is on the homepage or list recipes page' do
